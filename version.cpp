c548ecb4bef1a2004dcff608a12e755589730aef
MathxStudio <allan_pan_mathx123@163.com>
2022-05-22 12:31:54 +0800

    version 1

---
+++ version.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <map>
+#include <vector>
+#include <cstring>
+#include <istream>
+#include <complex>
+#include <ctime>
+
+using namespace std;
+
+
+int main()
+{
+    cout << 'version 1' << endl;
+    return 0;
+}
